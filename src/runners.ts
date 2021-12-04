5802b4c0a73ac2d1b94d331259aedbf0732a61d6
Michael Palazzo <pzzo@protonmail.com>
2021-12-03 16:29:47 -0800

    Move puzzles to 'days' directory

--- src/runners.ts
+++ src/runners.ts
@@ -1,5 +1,5 @@
-import { run as day01 } from "./day01.ts"
-import { run as day02 } from "./day02.ts"
+import { run as day01 } from "./days/day01.ts"
+import { run as day02 } from "./days/day02.ts"
 
 type Runner = (input: Deno.Reader, options?: Record<string, unknown>) => void
 
