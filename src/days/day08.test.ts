9345b50b5b6dd7e58e78c38e49483964b298af8f
Michael Palazzo <pzzo@protonmail.com>
2021-12-11 17:43:25 -0800

    Day 8 - part two

--- src/days/day08.test.ts
+++ src/days/day08.test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts"
 import { createInputFromString, parseArgs } from "../input.ts"
-import { run } from "./day08.ts"
+import { run, getLightConfiguration, lightCode } from "./day08.ts"
 
 const INPUT = `be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
 edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
@@ -14,9 +14,25 @@ egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
 gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 `
 
-Deno.test("run - 1/4/7/8 occurrences (default)", async () => {
+Deno.test("getSignalPattern", () => {
+    const input = ['acedgfb', 'cdfbe', 'gcdfa', 'fbcad', 'dab', 'cefabd', 'cdfgeb', 'eafb', 'cagedb', 'ab']
+    const config = getLightConfiguration(input)
+    assertEquals(config.get(lightCode(input[0])), 8)
+    assertEquals(config.get(lightCode(input[1])), 5)
+    assertEquals(config.get(lightCode(input[2])), 2)
+    assertEquals(config.get(lightCode(input[3])), 3)
+    assertEquals(config.get(lightCode(input[4])), 7)
+    assertEquals(config.get(lightCode(input[5])), 9)
+    assertEquals(config.get(lightCode(input[6])), 6)
+    assertEquals(config.get(lightCode(input[7])), 4)
+    assertEquals(config.get(lightCode(input[8])), 0)
+    assertEquals(config.get(lightCode(input[9])), 1)
+})
+
+Deno.test("run", async () => {
     const input = createInputFromString(INPUT)
     const options = parseArgs()
-    const result = await run(input, options)
-    assertEquals(result, 26)
+    const { count1478, sum } = await run(input, options)
+    assertEquals(count1478, 26)
+    assertEquals(sum, 61229)
 })
--- src/days/day08.ts
+++ src/days/day08.ts
@@ -1,9 +1,100 @@
 import { Input, OptionSelector } from "../input.ts"
 
-export async function run(input: Input, _: OptionSelector) {
-    let count = 0
-    for await (const output of input.map((s) => s.split(' | ')[1]?.split(' ') ?? [])) {
-        count += output.filter(o => o.length === 2 || o.length === 3 || o.length === 4 || o.length === 7).length
+const A = 'a'.charCodeAt(0)
+
+interface InputLine {
+    signalPattern: string[]
+    outputValues: string[]
+}
+
+interface RunResult {
+    count1478: number,
+    sum: number
+}
+
+export function lightCode(s: string): number {
+    let code = 0
+    for (let i = 0; i < s.length; i++) {
+        code = code | (1 << (s.charCodeAt(i) - A))
     }
-    return count
+    return code
+}
+
+export function getLightConfiguration(values: string[]): Map<number, number> {
+    const lengths = values.reduce((map, c) => {
+        const list = map.get(c.length) ?? []
+        list.push(lightCode(c))
+        map.set(c.length, list)
+        return map
+    }, new Map<number, number[]>())
+
+    const config: {[index: number]: number} = {}
+    config[8] = lengths.get(7)?.[0] ?? 0
+    config[4] = lengths.get(4)?.[0] ?? 0
+    config[7] = lengths.get(3)?.[0] ?? 0
+    config[1] = lengths.get(2)?.[0] ?? 0
+    config[3] = lengths.get(5)?.find(code => (code & config[1]) === config[1]) ?? 0
+    config[9] = config[3] | config[4]
+    config[6] = lengths.get(6)?.find(code => {
+        const check = config[9] & ~config[1]
+        return code !== config[9] && (code & check) === check
+    }) ?? 0
+    config[5] = config[9] & config[6]
+    config[0] = lengths.get(6)?.find(code => code !== config[9] && code !== config[6]) ?? 0
+    config[2] = lengths.get(5)?.find(code => code !== config[3] && code !== config[5]) ?? 0
+
+    const result = new Map()
+    for (const k in config) {
+        result.set(config[k], Number.parseInt(k))
+    }
+    return result
+}
+
+function mapInput(s: string): InputLine {
+    const [ sp, ov ] = s.split(' | ')
+    return {
+        signalPattern: sp.split(' '),
+        outputValues: ov.split(' ')
+    }
+}
+
+function get1478Occurrences(line: InputLine): number {
+    return line.outputValues.filter(o => {
+        return o.length === 2 
+            || o.length === 3
+            || o.length === 4
+            || o.length === 7
+    }).length
+}
+
+function getOutputValue(line: InputLine): number {
+    const config = getLightConfiguration(line.signalPattern)
+    if (config.size !== line.signalPattern.length) {
+        console.warn(
+            'Signal pattern is missing from light configuration.',
+            line.signalPattern.map(l => [l, lightCode(l)]),
+            config
+        )
+    }
+
+    const number = line.outputValues.reduce((carry, value, index, values) => {
+        const code = lightCode(value)
+        if (!config.has(code)) {
+            throw new Error(`Lightcode not found in configuration: ${value}`)
+        }
+        const n = config.get(code) as number
+        const exp = values.length - (index + 1)
+        return carry + (n * Math.pow(10, exp))
+    }, 0)
+
+    return number
+}
+
+export async function run(input: Input, _: OptionSelector) {
+    const result: RunResult = { count1478: 0, sum: 0 }
+    for await (const line of input.map(mapInput)) {
+        result.count1478 += get1478Occurrences(line)
+        result.sum += getOutputValue(line)
+    }
+    return result
 }
